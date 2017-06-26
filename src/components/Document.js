556d278a5e7d80eb0911bb393f3434b987aa3ea9
Alex Higuera <alexandra.higuera@gmail.com>
2017-06-26 19:57:37 -0400

    Add content to individual content page

--- src/components/Document.js
+++ src/components/Document.js
@@ -11,12 +11,18 @@ export default class Document extends Component {
     console.log(documentId, document);
         return (
             <div>
-            <h2>Document {documentId}</h2>
-            <blockquote>{document.content}</blockquote>
+            <h2>Document: {document.title}</h2>
+
+            <h4> Lender: {document.lender} </h4>
+            <h4> Date Added: {document.dateAdded} </h4>
+            <h4> Type: {document.type} </h4>
+            <h4> Signed By: {document.signedBy} </h4>
+            <h4> Content: </h4>
+            <blockquote className="indiv-doc">{document.content}</blockquote>
+            
+
             <h3> <button type="submit" className="btn btn-wide btn-o btn-primary" onClick={() => { alert(documentId) }}>Sign</button></h3>
-            <pre>
-            {JSON.stringify(document, null, '  ')}
-            </pre>
+
           </div>
         )
     }
