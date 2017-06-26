871f6c8776ba8d0c84267cf3b096e57cd65a60bd
Alex Higuera <alexandra.higuera@gmail.com>
2017-06-26 19:38:39 -0400

    Temporarily remove Sign Validation

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -72,8 +72,6 @@ export default class Documents extends Component {
 
                     </div>
                 </div>
-
-                { this.renderDocuments() }
             </div>
         );
     }
