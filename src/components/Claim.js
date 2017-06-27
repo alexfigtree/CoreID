a90d86497cc3e5ee4a65077cb6eb0ba211f9b80b
Alex Higuera <alexandra.higuera@gmail.com>
2017-06-26 21:17:31 -0400

    Change h1 color; add checkboxes to Statement of Intent

--- src/components/Claim.js
+++ src/components/Claim.js
@@ -9,14 +9,21 @@ export default class Claim extends Component {
     	console.log(claimId, claim);
 		return (
 			<div>
-	            <h2>Claim: {claim.payload.fact} </h2>
+	            <h1>Claim: {claim.payload.fact} </h1>
 	            <h4> Signed: {claim.verified ? 'Yes' : 'No'} </h4>
                 <h4> Signed Date: {claim.signedDate} </h4>
                 <h4> Signed By: {claim.signedBy} </h4>
                 <h4> Validated: {claim.validated ? 'Yes' : 'No'} </h4>
                 <h4> Verified By: {claim.verifiedBy} </h4>
                 <h4> Rejected: {claim.rejected ? 'Yes' : 'No'} </h4>
-	            <h3> <button type="submit" className="btn btn-wide btn-o btn-primary" onClick={() => { alert(claimId) }}>Sign / Send</button></h3>
+
+                <h2 className="">Statement of Intent:</h2>
+	                <p>
+		                <h5><input type="checkbox"/> I certify that...</h5>  
+		                <h5><input type="checkbox"/> I understand that this is a...</h5> 
+		                <h5><input type="checkbox"/> I authorize my...</h5> 
+		            </p>    
+	            <h3> <button type="submit" className="btn btn-wide btn-o btn-primary" onClick={() => { alert('Are you sure you want to sign or send ' + claim.payload.fact + '?') }}>Sign / Send</button></h3>
           	</div>
 		)
     }
--- src/components/Document.js
+++ src/components/Document.js
@@ -11,17 +11,17 @@ export default class Document extends Component {
     console.log(documentId, document);
         return (
             <div className="col-md-12">
-                <h2>Document: {document.title}</h2>
+                <h1>Document: {document.title}</h1>
                 <div className='column-left col-md-6'>
                     <h4> Lender: {document.lender} </h4>
                     <h4> Date Added: {document.dateAdded} </h4>
                     <h4> Type: {document.type} </h4>
                     <h4> Signed By: {document.signedBy} </h4>
                     <h4> Content: </h4>
-                    <blockquote className="">{document.content}</blockquote>    
+                    <blockquote className="">{document.content}</blockquote>  
                 </div>
 
-                <h3> <button type="submit" className="btn btn-wide btn-o btn-primary" onClick={() => { alert('Are you sure you want to sign this document?') }}>Sign</button></h3>
+                <h3> <button type="submit" className="btn btn-wide btn-o btn-primary" onClick={() => { alert('Are you sure you want to sign ' + document.title + '?') }}>Sign</button></h3>
             </div>
         )
     }
