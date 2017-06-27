c1dcda260f1b2811f0de7316c43638ed9b6cc61a
Alex Higuera <alexandra.higuera@gmail.com>
2017-06-26 20:42:19 -0400

    Change styling, add content to individual docs page

--- src/components/Document.js
+++ src/components/Document.js
@@ -10,20 +10,19 @@ export default class Document extends Component {
     let document = documents.payload[documentId] || {};
     console.log(documentId, document);
         return (
-            <div>
-            <h2>Document: {document.title}</h2>
+            <div className="col-md-12">
+                <h2>Document: {document.title}</h2>
+                <div className='column-left col-md-6'>
+                    <h4> Lender: {document.lender} </h4>
+                    <h4> Date Added: {document.dateAdded} </h4>
+                    <h4> Type: {document.type} </h4>
+                    <h4> Signed By: {document.signedBy} </h4>
+                    <h4> Content: </h4>
+                    <blockquote className="">{document.content}</blockquote>    
+                </div>
 
-            <h4> Lender: {document.lender} </h4>
-            <h4> Date Added: {document.dateAdded} </h4>
-            <h4> Type: {document.type} </h4>
-            <h4> Signed By: {document.signedBy} </h4>
-            <h4> Content: </h4>
-            <blockquote className="indiv-doc">{document.content}</blockquote>
-            
-
-            <h3> <button type="submit" className="btn btn-wide btn-o btn-primary" onClick={() => { alert(documentId) }}>Sign</button></h3>
-
-          </div>
+                <h3> <button type="submit" className="btn btn-wide btn-o btn-primary" onClick={() => { alert('Are you sure you want to sign this document?') }}>Sign</button></h3>
+            </div>
         )
     }
 }
