710371b438d2ae9c965165d12710c9ee1210c9c5
Alex Higuera <alexandra.higuera@gmail.com>
2017-07-08 16:30:43 -0400

    Revert routing changes to App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import {
 } from 'react-router-dom';
 
 
-import Index from './components/Index';
+import Index from './Dashboard/index';
 import Sandbox from './components/Sandbox';
 import Profile from './components/Profile';
 import Documents from './components/Documents';
 import Signatures from './components/Signatures';
+
+import Document from './components/Document';
+import Claim from './components/Claim';
 import Claims from './components/Claims';
-
-
+import ClaimsCreate from './components/ClaimsCreate';
 
 import Sidebar from './components/Sidebar';
 import Header from'./components/Header';
@@ -30,12 +32,15 @@ class App extends Component {
                         <Header/>
                         <NavBar/>
                         <div className="main-content">
-                            <Route exact path='/' component={Index} />
+                            <Route exact path='/' component={Documents} />
                             <Route path='/sandbox' component={Sandbox} />
                             <Route path='/documents' component={Documents} />
+                            <Route path='/document/:id' component={Document} />
+                            <Route path='/claim/:id' component={Claim} />
                             <Route path='/profile' component={Profile} />
                             <Route path='/signatures' component={Signatures} />
                             <Route path='/claims' component={Claims} />
+                            <Route path='/claim-create' component={ClaimsCreate} />
                         </div>
                     </div>
 
