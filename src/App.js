5b0bedc0fb492aa0383da07e1e8d76da21376876
Alex Higuera <alexandra.higuera@gmail.com>
2017-07-09 01:26:11 -0400

    Docs folder cleanup

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ import Sidebar from './components/Sidebar';
 import Header from'./components/Header';
 import NavBar from './components/NavBar';
 
+import jsonfile from 'jsonfile'
+
 class App extends Component {
 
     render() {
